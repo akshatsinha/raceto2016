f61f130680cc957a1091488c4521857e9057f78a
Akshat Sinha <asinha@auction.com>
2015-08-19 00:26:37 -0700

    state page side dropdown

--- client/app/state/state.controller.js
+++ client/app/state/state.controller.js
@@ -1,7 +1,16 @@
 'use strict';
 
 angular.module('raceApp')
-  .controller('StateCtrl', function ($stateParams) {
-    var stc = this;
-    stc.state = $stateParams.state;
-  });
+    .controller('StateCtrl', function ($stateParams) {
+        var stc = this;
+        stc.state = $stateParams.state;
+    })
+    .controller('LeftStateListCtrl', function ($stateParams, statesSvc) {
+        var lslc = this;
+        lslc.stateMenuOpen = false;
+        lslc.state = $stateParams.state;
+        lslc.stateList = statesSvc.getStatesList();
+        lslc.openStateMenu = function() {
+            lslc.stateMenuOpen = !lslc.stateMenuOpen;
+        }
+    });
