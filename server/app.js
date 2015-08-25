9908cea8fed2f8ed47ada4d6ac6994682ea1ecf6
Akshat Sinha <asinha@auction.com>
2015-08-24 21:35:56 -0700

    Added middleware for no-access-control-origin

--- server/app.js
+++ server/app.js
@@ -13,14 +13,14 @@ var config = require('./config/environment');
 var app = express();
 app.use(function (req, res, next) {
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://raceto2016.com:9000');
+    res.header('Access-Control-Allow-Origin', 'http://raceto2016.com:9000');
     // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Credentials', false);
     next();
 });
 
